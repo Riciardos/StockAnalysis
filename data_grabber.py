11b5ca6d777e736047fda64af3c21a895d262926
Ricaro Schuller <ricardoschuller90@gmail.com>
2016-09-13 16:16:00 +0100

    Secondary commit

--- data_grabber.py
+++ data_grabber.py
@@ -8,8 +8,17 @@ def file_grabber(url):
     response = urllib.request.urlopen(url)
     data  = response.read()
     text = data.decode('utf-8')
-    print(text)
+    # print(text)
+
+def grab_all():
+    for i in aex_links:
+        file_grabber(aex_links[i])
 
 
-for i in aex_links:
-    file_grabber(aex_links[i])
+def csv_to_list(path_to_csv_file):
+
+    with open(path_to_csv_file, 'r') as f:
+        reader = csv.reader(f)
+        data_list = list(reader)
+
+    return data_list
--- fourier_analysis.py
+++ fourier_analysis.py
@@ -1,29 +1,41 @@
 ## FOURIER ANALYSIS OF STOCKS, INDIVIDUAL AND INDEX
 
 
+import numpy as np
 
-import math
 
-
-def fourier_transform(data, return_type ="fft"):
+def discrete_fourier_transform(data, return_type ="dft"):
     # Takes in market data and returns a list of constants of the Fourier series
     # Keyword argument:
-    #                   return_type : fft for full transform, the same length
-    # TODO : FFT
-    return_dic = {"fft":0,"cst":1}
-    return return_dic[return_type]
+    #                   return_type : dft for full transform, the same length
+    #                                 cst for constants
+    # TODO : DFT
 
-def calc_ftt_error(data, fttcst):
+    data_length = len(data)
+    f_k = np.zeros_like(data)
+    for k in range(0,data_length):
+        fsum = 0
+        omega = 2*k*np.pi/data_length
+        for n in range(0,data_length):
+            fsum += data[n]*np.cos(n*omega)
+        f_k[k] = fsum
+
+    type_dic = {"dft":0,"cst":1}
+    return_dic = [0,f_k]
+    return return_dic[type_dic[return_type]]
+
+
+def calc_ft_error(data, ftcst, return_type ="abs"):
     # Just calculate the error between actual data and Fourier series.
     # Return dictionary with info ( absolute/relative error)
-    #TODO : everything
+    # TODO : everything
 
-    return None
+    return_dic = {"abs":0,"rel":1}
+    return return_dic[return_type]
+
 
 def minimize_error(data, type = "absolute"):
 
     data_fft_cst = fourier_transform(data)
 
     err = calc_ftt_error(data, data_fft_cst)
-
-    
